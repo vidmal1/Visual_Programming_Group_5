298d81aeba1953a12dcbc5c3a2d03c208005aead
Sandun Deshan <deshansandun961@gmail.com>
2026-06-24 21:12:22 +0530

    Add dashboard summary with real session data

---
+++ FocusTrack/FocusTrack.Business/DTOs/AppUsageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FocusTrack.Business.DTOs
+{
+    public class AppUsageDto
+    {
+        public string ApplicationName { get; set; } = string.Empty;
+
+        public int TotalSeconds { get; set; }
+
+        public string DurationText { get; set; } = string.Empty;
+    }
+}
---
+++ FocusTrack/FocusTrack.Business/DTOs/DashboardSummaryDto.cs
@@ -0,0 +1,17 @@
+﻿namespace FocusTrack.Business.DTOs
+{
+    public class DashboardSummaryDto
+    {
+        public int TotalSeconds { get; set; }
+
+        public string TotalDurationText { get; set; } = string.Empty;
+
+        public int SessionCount { get; set; }
+
+        public string MostUsedApplication { get; set; } = "N/A";
+
+        public string MostUsedDurationText { get; set; } = "0s";
+
+        public List<AppUsageDto> AppUsages { get; set; } = new List<AppUsageDto>();
+    }
+}
