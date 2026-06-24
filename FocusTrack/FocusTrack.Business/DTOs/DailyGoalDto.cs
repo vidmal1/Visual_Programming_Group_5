817305dfe6bdc7f46a5ec364e530579d766cb198
Sandun Deshan <deshansandun961@gmail.com>
2026-06-24 23:34:27 +0530

    Add settings management

---
+++ FocusTrack/FocusTrack.Business/DTOs/DailyGoalDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FocusTrack.Business.DTOs
+{
+    public class DailyGoalDto
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = string.Empty;
+
+        public int GoalMinutes { get; set; }
+    }
+}
