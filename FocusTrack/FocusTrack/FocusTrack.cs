0a58c6f009f71302e34e9d17cbbfb41fb44af2c0
Gimhana Menake <gimhanamenaka@icloud.com>
2026-07-01 14:40:51 +0530

    initialize

---
+++ FocusTrack/FocusTrack/FocusTrack.cs
@@ -0,0 +1,15 @@
+namespace FocusTrack
+{
+    public partial class FocusTrack : Form
+    {
+        public FocusTrack()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
