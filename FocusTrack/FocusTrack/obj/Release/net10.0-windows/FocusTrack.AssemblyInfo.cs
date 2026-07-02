f1340576ae3570c32a9e50e03131895b71f3d836
Sandun Deshan <deshansandun961@gmail.com>
2026-07-02 15:38:58 +0530

    Add storage helper for SQLite sessions

--- FocusTrack/FocusTrack/obj/Release/net10.0-windows/FocusTrack.AssemblyInfo.cs
+++ FocusTrack/FocusTrack/obj/Release/net10.0-windows/FocusTrack.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FocusTrack")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa24991290ec8ae0ce33592962ee7867e6f235ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FocusTrack")]
