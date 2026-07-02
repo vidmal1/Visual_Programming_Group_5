3de596763c8dc5cd483ad68fcfe5f6bb0f5ff34b
Sandun Deshan <deshansandun961@gmail.com>
2026-07-02 16:02:41 +0530

    Add foreground window tracking services

--- FocusTrack/FocusTrack/obj/Release/net10.0-windows/FocusTrack.AssemblyInfo.cs
+++ FocusTrack/FocusTrack/obj/Release/net10.0-windows/FocusTrack.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FocusTrack")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b14a5a406128d74314ed9f2f066239f6ed7367ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FocusTrack")]
